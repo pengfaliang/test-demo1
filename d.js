b95a759bc50815c3ce12ecbefca6c6683ba7cd64
pengfaliang <1677618445@qq.com>
2026-05-23 14:51:32 +0800

    Fix/d (#7)
    * fix: 修复d模块函数实现
    
    * fix: 新增了导出fn1和fn2
    
    * Refactor functions to use arrow syntax and const

--- d.js
+++ d.js
@@ -1,6 +1,6 @@
-function fn1(...args) {
-  console.log(...args);
-}
-function fn2(...args) {
-  console.log(...args);
+export const fn1 = (...args) => {
+  console.log('fn1', ...args);
+};
+export const fn2 = (...args) => {
+  console.log('fn2', ...args);
 }
